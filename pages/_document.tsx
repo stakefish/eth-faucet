de8ffc24c9022f48d34b0a36e9704b9b07a98852
mateusz <mateusz@stake.fish>
2022-12-16 13:29:36 +0400

    feat: whitelist users using http referer

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,6 @@
+import { GetServerSideProps } from "next"
 import { Html, Head, Main, NextScript } from "next/document"
+import { setCookies } from "cookies-next"
 
 const Document = () => (
   <Html>
@@ -13,5 +15,29 @@ const Document = () => (
     </body>
   </Html>
 )
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const referer = req.headers.referer || ""
+
+  const whitelistedUrls = ["https://stakefish-website-main.vercel.app", "http://localhost:3000"]
+  const originRegexp = new RegExp(
+    "https://[^.]+.stake.fish|https://stakefish-website-[^.]+.vercel.app|https://.*.stakefish.link",
+    "gi"
+  )
+
+  const isWhitelistedUrl = whitelistedUrls.reduce((result: boolean, url: string) => {
+    if (result) {
+      return true
+    }
+
+    return referer.startsWith(url)
+  }, false)
+
+  if (isWhitelistedUrl || originRegexp.test(referer)) {
+    setCookies("stakefish_referer", "1", { req, res, maxAge: 60 * 7 * 24 })
+  }
+
+  return { props: {} }
+}
 
 export default Document
